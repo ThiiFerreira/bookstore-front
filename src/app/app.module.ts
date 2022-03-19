b76e45a228efe7c292228dece4d7a3efd14d26f9
Thiago Ferreira <thiago.ferreira@aluno.ifsp.edu.br>
2022-03-19 19:04:03 -0300

    Criando o componente categoria-create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
+    CategoriaCreateComponent,
   ],
   imports: [
     BrowserModule,
