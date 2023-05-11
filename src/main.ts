8a64b78670b62ac7b4bd3589cb3cd41efc93e82b
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2023-05-11 15:54:40 -0300

    correção de bug ao carregar pagina home_2

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { Injectable, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -10,15 +10,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-const start = () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
-};
-
-if (environment.production) {
-  enableProdMode();
-  window.location.reload();
-} else {
-  start();
-}
